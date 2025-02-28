80e1c103433f3badd37cb5feab483302b1f5db07
CaioVKodato <caiovictorkodatot@gmail.com>
2025-02-28 16:19:30 -0300

    Padrão DTO para Tecnico

--- src/main/java/com/caiokodato/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/caiokodato/helpdesk/controllers/TecnicoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caiokodato.helpdesk.entities.Tecnico;
+import com.caiokodato.helpdesk.entities.dtos.TecnicoDTO;
 import com.caiokodato.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoController {
     private TecnicoService service;
     
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
 }
