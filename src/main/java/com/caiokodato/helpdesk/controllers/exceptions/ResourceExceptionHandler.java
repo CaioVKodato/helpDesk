b08d31cc737d00636e5d78cfc8bc64f768dfccde
CaioVKodato <caiovictorkodatot@gmail.com>
2025-03-01 11:05:54 -0300

    Exception para CPF e Email ja cadastrados

--- src/main/java/com/caiokodato/helpdesk/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/caiokodato/helpdesk/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import com.caiokodato.helpdesk.services.execptions.DataIntegrityViolationException;
 import com.caiokodato.helpdesk.services.execptions.ObjectnotFoundExecption;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,5 +15,10 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandartError> objectNotFoundException(ObjectnotFoundExecption ex, HttpServletRequest request ) {
         StandartError error = new StandartError(System.currentTimeMillis(),HttpStatus.NOT_FOUND.value(),"Object Not Found",ex.getMessage(),request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    public ResponseEntity<StandartError> DataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request ) {
+        StandartError error = new StandartError(System.currentTimeMillis(),HttpStatus.BAD_REQUEST.value(),"Violação de dados",ex.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/caiokodato/helpdesk/services/TecnicoService.java
+++ src/main/java/com/caiokodato/helpdesk/services/TecnicoService.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.caiokodato.helpdesk.entities.Pessoa;
 import com.caiokodato.helpdesk.entities.Tecnico;
 import com.caiokodato.helpdesk.entities.dtos.TecnicoDTO;
+import com.caiokodato.helpdesk.repositories.PessoaRepository;
 import com.caiokodato.helpdesk.repositories.TecnicoRepository;
+import com.caiokodato.helpdesk.services.execptions.DataIntegrityViolationException;
 import com.caiokodato.helpdesk.services.execptions.ObjectnotFoundExecption;
 
 @Service
@@ -16,6 +19,9 @@ public class TecnicoService {
     
     @Autowired
     private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
@@ -28,8 +34,21 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
-        Tecnico newObj = new Tecnico(objDTO);
-        return tecnicoRepository.save(newObj);
+        validaPorCpfEEmail(objDTO);
+                Tecnico newObj = new Tecnico(objDTO);
+                return tecnicoRepository.save(newObj);
+            }
+        
+     private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF ja cadastrado na base de dados!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("E-mail ja cadastrado na base de dados!");
+        }
     }
 
    
